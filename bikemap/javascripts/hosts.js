2a4e8ac10ffa79c1d0463c1e86818b34ad51baa0
Curtis Holmes <csh22@case.edu>
2018-01-01 10:55:41 -0500

    step 1

---
+++ bikemap/javascripts/hosts.js
@@ -0,0 +1,20 @@
+var hosts = new bikeRoute(host_data);
+
+function hostMarkers(host_data) {
+  this.hostMarkers = createHostMarkers(host_data);
+}
+
+hostMarkers.prototype.show = function(map) {
+  this.hostMarkers.forEach(hostMarker => hostMarker.setMap(map));
+}
+
+function createHostMarkers(host_data) {
+  return host_data.map(coordinate => createHostMarker(coordinate));
+}
+
+function createHostMarker(coordinate) {
+  return marker = new google.maps.Marker({
+    position: new google.maps.LatLng(coordinate[0], coordinate[1]),
+    title: 'Host'
+  });
+}
